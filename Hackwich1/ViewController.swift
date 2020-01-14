b6fc14fef3396aca5580214c3e767c6798d30be0
Oliver <ocjr97@gmail.com>
2020-01-14 10:37:27 -1000

    Class 1 01/14/2020 Hackwich1

---
+++ Hackwich1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hackwich1
+//
+//  Created by CM Student on 1/14/20.
+//  Copyright © 2020 Oliver Corpuz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
